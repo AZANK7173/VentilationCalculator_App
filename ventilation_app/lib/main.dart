00a3bd841e2502748216e8a0973c36da7580a5d8
febagni <febagni@usp.br>
2024-05-18 20:35:14 +0200

    :sparkles: Add second screen, with some bugs

--- ventilation_app/lib/main.dart
+++ ventilation_app/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 // width = 393.0
 // height = 753.0
 
 void main() {
-  runApp(MyApp());
+  runApp(Instructions());
 }
 
 class MyApp extends StatelessWidget {
@@ -28,9 +27,10 @@ class MyApp extends StatelessWidget {
               const SizedBox(
                   height: 80.0), // Add spacing between text and button
               Image.asset(
-                '../assets/entry.jpg',
+                'assets/entry.jpg',
                 width: screenHeight * 0.35,
               ),
+
               const SizedBox(
                   height: 20.0), // Add spacing between image and text
               const Text(
@@ -71,20 +71,24 @@ class MyApp extends StatelessWidget {
 
               ElevatedButton(
                 onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Instructions()),
+                  );
                   // Navigate to the next page (to be implemented later)
                   // You can use Navigator.push or a similar method here
                 },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 45, 133, 185),
+                    minimumSize: const Size(350, 55.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0))),
                 child: const Text(
                   'Let\'s get started',
                   style: TextStyle(
                     color: Color.fromARGB(255, 242, 244, 247),
                   ),
                 ),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 45, 133, 185),
-                    minimumSize: const Size(350, 55.0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0))),
               ),
             ],
           ),
@@ -119,6 +123,191 @@ class MyApp extends StatelessWidget {
                       style: TextStyle(
                         fontSize: 15.0,
                         color: Color.fromARGB(255, 16, 24, 40),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Instructions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    debugPrint('Screen Height: $screenHeight');
+    debugPrint('Screen Width: $screenWidth');
+
+    return MaterialApp(
+      title: 'Ventilation Calculator',
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 7, 59, 91), // Set blue background
+        body: 
+          Center(
+          child: Container(
+            alignment: Alignment.center, // Align the content to the center of the container
+            width: screenWidth * 0.90, // Set your desired width
+            child: 
+            Row(
+              children: [
+              const Flexible(
+                child:SizedBox(width: 40,)
+              ),
+              Flexible(
+                child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center content vertically
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Align content to the left horizontally
+                children: [
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const Text(
+                    'This tool estimates: ',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 109, 29),
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(
+                    height: 20.0, // Add spacing between image and text
+                  ),
+                  const Text(
+                    '1. The ventilation rate of your space.',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Color.fromARGB(255, 242, 244, 247),
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(
+                    height: 10.0, // Add spacing between image and text
+                  ),
+                  RichText(
+                    textAlign: TextAlign.left,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '2. The maximum occupancy ',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Color.fromARGB(255, 242, 244, 247),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              'based on WHO recommended requirements. Please note that reccomended occupancy only considers ventilation rate; physical distancing should always be ensured.',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Color.fromARGB(255, 242, 244, 247),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10.0, // Add spacing between image and text
+                  ),
+                  RichText(
+                    textAlign: TextAlign.left,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '3. The potential ventilation gap, ',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Color.fromARGB(255, 242, 244, 247),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'considering: ',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Color.fromARGB(255, 242, 244, 247),
+                          ),
+                        ),
+                        TextSpan(
+                          text: '\n \u2022 Desired room occupancy. \n \u2022 Actual estimated ventilation rate.',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Color.fromARGB(255, 242, 244, 247),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0, // Add spacing between image and text
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigate to the next page (to be implemented later)
+                      // You can use Navigator.push or a similar method here
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 255, 109, 29),
+                        minimumSize: const Size(350, 55.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0))),
+                    child: const Text(
+                      'Got it!',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 242, 244, 247),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+              const Flexible(
+                child:SizedBox(
+                width: 40,
+              )),
+              ]
+            ),
+          ),
+        ),
+        bottomNavigationBar: SizedBox(
+          height: 60.0,
+          child: Center(
+            child: Container(
+              alignment: Alignment.center,
+              width: screenWidth * 0.90, // Set your desired width
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'ⓘ We use formulas by the ',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Color.fromARGB(255, 242, 244, 247),
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'World Health Organization (WHO)',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Color.fromARGB(255, 242, 244, 247),
+                        fontWeight: FontWeight.bold, // Make this part bold
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' to calculate ventilation rates.',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Color.fromARGB(255, 242, 244, 247),
                       ),
                     ),
                   ],
