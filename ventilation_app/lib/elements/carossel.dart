4683b4e0229ac3c161a60d5f24ac0de5518647d6
AZANK7173 <felipeazank@gmail.com>
2024-06-06 17:37:33 +0200

    :iphone: Fix Caroussel

--- ventilation_app/lib/elements/carossel.dart
+++ ventilation_app/lib/elements/carossel.dart
@@ -3,12 +3,16 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 class CarouselImageSlider extends StatefulWidget {
   final List<String> imageAssetPaths;
+  final List<String> imageTexts; // Add a list for image text
   final double height;
+  final double borderRadius; // Add a parameter for border radius
 
   const CarouselImageSlider({
     Key? key,
     required this.imageAssetPaths,
-    this.height = 200.0,
+    required this.imageTexts, // Pass the text list
+    this.height = 170.0,
+    this.borderRadius = 15.0, // Set default border radius
   }) : super(key: key);
 
   @override
@@ -22,24 +26,44 @@ class _CarouselImageSliderState extends State<CarouselImageSlider> {
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-      items: widget.imageAssetPaths.map((imagePath) {
+      items: widget.imageAssetPaths.asMap().entries.map((entry) {
+        int index = entry.key;
+        String imagePath = entry.value;
         return Builder(
-          builder: (BuildContext context) => Image.asset(
-            imagePath,
-            fit: BoxFit.cover,
-            height: widget.height,
-            errorBuilder: (context, error, stackTrace) =>
-                const Center(child: CircularProgressIndicator()),
+          builder: (BuildContext context) => ClipRRect(
+            // Wrap with ClipRRect
+            borderRadius:
+                BorderRadius.circular(widget.borderRadius), // Set border radius
+            child: Stack(
+              children: [
+                Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                  height: widget.height,
+                  errorBuilder: (context, error, stackTrace) =>
+                      const Center(child: CircularProgressIndicator()),
+                ),
+                Positioned(
+                  bottom: 10.0, // Adjust padding (optional)
+                  left: 10.0, // Adjust padding (optional)
+                  child: Text(
+                    widget.imageTexts[index],
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       }).toList(),
       carouselController: CarouselController(),
       options: CarouselOptions(
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 3),
         height: widget.height,
         aspectRatio: 16 / 9, // Adjust aspect ratio as needed
-        viewportFraction: 0.8, // Adjust visible portion of each image
-        enableInfiniteScroll: widget.imageAssetPaths.length >
-            1, // Enable looping for multiple images
+        viewportFraction: 1, // Adjust visible portion of each image
+        enableInfiniteScroll: widget.imageAssetPaths.length > 1,
         onPageChanged: (index, reason) =>
             setState(() => _currentImageIndex = index),
       ),
--- ventilation_app/lib/screens/input_nat_more_opens.dart
+++ ventilation_app/lib/screens/input_nat_more_opens.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ventilation_app/elements/upper_navigation_bar.dart';
-import 'package:flutter/gestures.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:ventilation_app/elements/slider_and_switch.dart';
 import 'package:ventilation_app/elements/texts_and_buttons.dart';
 import 'package:ventilation_app/elements/carossel.dart';
 
@@ -38,7 +35,7 @@ class InputNatMoreOpens extends StatelessWidget {
                     onPressed: () {
                       Navigator.of(context).pushNamed('/input_nat_2');
                     },
-                    text: 'Next',
+                    text: 'Back to Openings',
                   ),
                   const SizedBox(height: 30.0),
                 ],
@@ -86,6 +83,12 @@ class InputNatMoreOpens extends StatelessWidget {
           ),
           const SizedBox(height: 20.0),
           const CarouselImageSlider(
+            imageTexts: [
+              'Horizontal and Vertical',
+              'Slide',
+              'Tilt and Turn',
+              'Top and Bottom',
+            ],
             imageAssetPaths: [
               'assets/typology/horizontal_vertical.jpg',
               'assets/typology/slide.jpg',
@@ -150,37 +153,6 @@ class InputNatMoreOpens extends StatelessWidget {
     );
   }
 
-  Widget _buildHyperLinkText(String boldText1, String boldText2) {
-    return RichText(
-      text: TextSpan(
-        children: [
-          TextSpan(
-            text: boldText1,
-            style: const TextStyle(
-              fontSize: 15.0,
-              color: Color.fromARGB(255, 102, 112, 133),
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          TextSpan(
-            text: boldText2,
-            style: const TextStyle(
-              fontSize: 15.0,
-              color: Color.fromARGB(255, 255, 109, 29),
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.underline,
-            ),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                launchUrl(
-                    Uri.parse('https://www.youtube.com/watch?v=KWP8pQnGoBw'));
-              },
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget _buildRichTextContent(
       String normalText1, String boldText, String normalText2) {
     return RichText(
@@ -212,112 +184,4 @@ class InputNatMoreOpens extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildWindowInputWidget() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const TextEntry(
-          myColor: Color.fromARGB(255, 67, 150, 199),
-          text: 'Door/window number 1',
-          fontSize: 17.0,
-          fontWeight: FontWeight.bold,
-        ),
-        //unica coisa que vai mudar na hora de fazer a logica de adicionar mais janelas
-        const SizedBox(height: 10.0),
-        _buildInput1('Number of openings with the same size: '),
-        const SizedBox(height: 20.0),
-        Slider0To100(dragText: 'How much do you usually open it?'),
-        const SizedBox(height: 20.0),
-        const TextEntry(
-            myColor: Color.fromARGB(255, 102, 112, 133),
-            text: 'Enter dimensions',
-            fontSize: 15,
-            fontWeight: FontWeight.normal),
-        const SizedBox(height: 20.0),
-        const DimensionInputRow(
-            dropdownItems: ['meters', 'inches', 'centimeters'],
-            labelText: 'Length'),
-        const SizedBox(height: 10.0),
-        const DimensionInputRow(
-            dropdownItems: ['meters', 'inches', 'centimeters'],
-            labelText: 'Height'),
-        const SizedBox(height: 10.0),
-        Switcher(
-          switchText: 'Does it have a mosquito net?',
-        ),
-      ],
-    );
-  }
-
-  Widget _buildInput1(String inputText) {
-    return Row(
-      children: [
-        TextEntry(
-          myColor: const Color.fromARGB(255, 102, 112, 133),
-          text: inputText,
-          fontSize: 15,
-          fontWeight: FontWeight.bold,
-        ),
-        const SizedBox(width: 10.0),
-        const Expanded(
-          child: SizedBox(
-            height: 30,
-            child: TextField(
-              style:
-                  TextStyle(fontSize: 12.0, height: 2.0, color: Colors.black),
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  borderSide:
-                      BorderSide(color: Color.fromARGB(255, 214, 220, 220)),
-                ),
-                contentPadding: EdgeInsets.fromLTRB(0, 30, 0, 37),
-                isDense: true,
-                labelText: '',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-Widget _buildOpeningImage(double screenWidth, String filepath) {
-  return ClipRRect(
-    borderRadius: BorderRadius.circular(15.0),
-    child: Image.asset(
-      filepath,
-      width: screenWidth * 0.90,
-    ),
-  );
-}
-
-Widget _buildAddNewWindowButton(BuildContext context) {
-  return Center(
-    child: ElevatedButton(
-      onPressed: () {
-        //TODO add new window
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        minimumSize: const Size(350, 55.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          side: const BorderSide(
-            color: Color.fromARGB(255, 45, 133, 185),
-          ),
-        ),
-      ),
-      child: const Text(
-        '+ Add new openning type',
-        style: TextStyle(
-          color: Color.fromARGB(255, 45, 133, 185),
-        ),
-      ),
-    ),
-  );
 }
